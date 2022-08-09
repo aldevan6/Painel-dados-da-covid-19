65e50951886c7ea0d504ce2709c6798fbae11a97
Aldevan Silva <aldevan6@gmail.com>
2022-08-08 23:55:23 -0300

    Retirou o comportamento padrao de envio do form

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,6 @@
 // URL da API: https://coronavirus-19-api.herokuapp.com/countries
+
+document.querySelector('.busca').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    
+});
